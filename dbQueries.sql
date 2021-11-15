94d59c2eeba010b96933450b6d660af97c8b8c77
dalealabastro <90293022+dalealabastro@users.noreply.github.com>
2021-11-15 12:33:56 -0800

    Update dbQueries.sql

--- dbQueries.sql
+++ dbQueries.sql
@@ -4,72 +4,83 @@ SELECT *
 FROM    BulletData;
 
 -- Get queried data
--- Hoever this will hold static data for now.
+-- Hoever this will hold static data for now. 1
 SELECT  *
 FROM    BulletData
 WHERE   b_caliburKey = "7.62x25mm"
 ORDER BY b_dmg DESC;
 
--- Get Tracer Rounds
+-- Get Tracer Rounds 2
 SELECT *
 FROM BulletData
 WHERE   b_special LIKE "%Tracer%";
 
--- Get all Vendors Info
+-- Get all Vendors Info 3
 SELECT *
 FROM    Vendors;
 
--- Get Bullets Vendors Sell
+-- Get Bullets Vendors Sell 4
 SELECT *
 FROM Vendors, BulletData
 WHERE   v_vendorKey = b_vendorKey
 
--- SMG Caliburs
+-- SMG Caliburs 5
 SELECT *
 FROM BulletData, Caliburs
 WHERE   c_caliburKey = b_caliburKey
 AND     c_gunType LIKE "%SMGs%";
 
--- Pistol Caliburs
+-- Pistol Caliburs 6
 SELECT *
 FROM BulletData, Caliburs
 WHERE   c_caliburKey = b_caliburKey
 AND     c_gunType LIKE "%Pistol%";
 
--- AR Caliburs
+-- AR Caliburs 7
 SELECT *
 FROM BulletData, Caliburs
 WHERE   c_caliburKey = b_caliburKey
 AND     c_gunType LIKE "%AR%";
 
--- Assault Carbine Caliburs
+-- Assault Carbine Caliburs 8
 SELECT *
 FROM BulletData, Caliburs
 WHERE   c_caliburKey = b_caliburKey
 AND     c_gunType LIKE "%AC%";
 
--- Marksman Rifles Caliburs
+-- Marksman Rifles Caliburs 9
 SELECT *
 FROM BulletData, Caliburs
 WHERE   c_caliburKey = b_caliburKey
 AND     c_gunType LIKE "%DMRs%";
 
--- Sniper Rifles Caliburs
+-- Sniper Rifles Caliburs 10
 SELECT *
 FROM BulletData, Caliburs
 WHERE   c_caliburKey = b_caliburKey
 AND     c_gunType LIKE "%Sniper Rifle%";
 
--- SG Shells - Buckshot
+-- SG Shells - Buckshot 11
 SELECT *
 FROM BulletData, Caliburs
 WHERE   c_caliburKey = b_caliburKey
 AND     c_gunType LIKE "%SG%"
 AND     b_name LIKE "%Buckshot%";
 
--- SG Shells - Slugs
+-- SG Shells - Slugs 12
 SELECT *
 FROM BulletData, Caliburs
 WHERE   c_caliburKey = b_caliburKey
 AND     c_gunType LIKE "%SG%"
 AND     b_name LIKE "%Slug%";
+
+
+-- Grenade throwable -- 13
+SELECT *
+FROM GrenadeData, Vendors, Prices
+WHERE g_name = p_calName
+AND  v_vendorKey = p_vendorKey
+
+-- High dmg for cheapest price --
+
+SELECT *
